f619179a070a679bdd4faad0c42ba5cb67399539
Mohamed Bushra <mohamed.bushra@reedonline.co.uk>
2020-10-14 19:09:14 +0100

    Complete beginner contact us test challenge

---
+++ features/pageobjects/bookRoom.page.js
@@ -0,0 +1,10 @@
+import BasePage from './basePage.page';
+
+export default class BookRoomPage extends BasePage {
+    /**
+     * define selectors using getter methods
+     */
+    
+}
+
+export const bookRoomPage = new BookRoomPage();
